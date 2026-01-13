d64334a83c231ef1e919a1330730f72f6fc0a6cb
Isutomu <isutomu@gmail.com>
2026-01-13 12:02:31 -0300

    :sparkles: feat: Implement routes

---
+++ src/routes/App.tsx
@@ -0,0 +1,45 @@
+// 3rd Party Modules
+import { AnimatePresence } from "motion/react";
+import React, { createContext, useState } from "react";
+import { Navigate, useLocation, useOutlet } from "react-router-dom";
+
+// Local Modules
+import { Error } from "../components/Error";
+
+// Exportable Constants
+type ErrorContextValue = {
+  error: string | null;
+  setError: (value: string) => void;
+} | null;
+export const ErrorContext = createContext<ErrorContextValue>(null);
+
+// Exportable Component
+export const App = () => {
+  const [error, setError] = useState<string | null>(null);
+  const location = useLocation();
+  const outlet = useOutlet();
+
+  const simplifiedLocation = location.pathname.split("/")[1];
+  if (simplifiedLocation === "") {
+    return <Navigate to="/app" />;
+  }
+
+  return (
+    <>
+      <Error error={error} />
+      <ErrorContext.Provider
+        value={{
+          error,
+          setError: (value) => {
+            setError(value);
+            setTimeout(() => setError(null), 4000);
+          },
+        }}
+      >
+        <AnimatePresence mode="wait">
+          {outlet && React.cloneElement(outlet, { key: simplifiedLocation })}
+        </AnimatePresence>
+      </ErrorContext.Provider>
+    </>
+  );
+};
---
+++ src/routes/index.tsx
@@ -0,0 +1,14 @@
+// 3rd Party Modules
+import { createBrowserRouter } from "react-router-dom";
+
+// Local Modules
+import { App } from "./App";
+import Boilerplate from "../pages/Boilerplate/Boilerplate";
+
+export const routes = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [{ path: "/app", element: <Boilerplate /> }],
+  },
+]);
