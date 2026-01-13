739563e6708243a396ae2171ffaf04fc461b7a66
Isutomu <isutomu@gmail.com>
2026-01-13 10:36:15 -0300

    :wrench: chore: Update vite config
    Makes the transition between development and production builds to be
    automatic in terms of base url.

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,14 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+import type { UserConfig } from "vite";
+
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: "https://isutomu.github.io/odin-book-frontend-top/",
+export default defineConfig(({ mode }) => {
+  const config: UserConfig = { plugins: [react()] };
+  if (mode === "production") {
+    config["base"] = "https://isutomu.github.io/odin-book-frontend-top/";
+  }
+
+  return config;
 });
