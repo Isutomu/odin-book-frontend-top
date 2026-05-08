9fc54c4e9c18dae9885018b6a636e0132e364091
Isutomu <isutomu@gmail.com>
2026-05-08 17:51:55 -0300

    :sparkles: feat: Complement route sensible session
    Now including the "Protected Routes".

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -6,6 +6,8 @@ import { App } from "./App";
 import { Login } from "../pages/Login/Login";
 import { UnprotectedRoute } from "./UnprotectedRoute";
 import { Signup } from "../pages/Signup/Signup";
+import { ProtectedRoute } from "./ProtectedRoute";
+import { Homepage } from "../pages/Homepage/Homepage";
 
 export const routes = createBrowserRouter(
   [
@@ -23,8 +25,9 @@ export const routes = createBrowserRouter(
       ],
     },
     {
-      element: <div></div>,
-      children: [{ path: "/app", element: <div></div> }],
+      path: "/app",
+      element: <ProtectedRoute />,
+      children: [{ path: "/app", element: <Homepage /> }],
     },
   ],
   { basename: import.meta.env.BASE_URL },
